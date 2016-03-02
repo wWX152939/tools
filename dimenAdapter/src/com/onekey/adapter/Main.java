741bf642659963ea58e98ccc841421f3a48a7137
wWX152939 <www87538089@qq.com>
2016-03-02 11:42:00 +0800

    添加注释

--- dimenAdapter/src/com/onekey/adapter/Main.java
+++ dimenAdapter/src/com/onekey/adapter/Main.java
@@ -1,5 +1,10 @@
 package com.onekey.adapter;
 
+/**
+ * Android dimen适配，默认dimens.xml只针对1280x736，dpi为213的设备。
+ * @author onekey
+ *
+ */
 public class Main {
 	
 	public static void main(String[] args) {
